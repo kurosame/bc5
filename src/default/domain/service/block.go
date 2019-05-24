09f227d68a246cefb4dbf84caa5772cba2de3c93
kurosame <kurosame1227@gmail.com>
2019-05-24 20:52:57 +0900

    add: CreateBlock func in domain.service

--- src/default/domain/service/block.go
+++ src/default/domain/service/block.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"strconv"
+	"time"
 
 	"default/domain/model"
 )
@@ -11,9 +12,16 @@ import (
 // HashBlock is extends model.Block
 type HashBlock model.Block
 
-// CreateHash create SHA-256 hash based on Block
+// CreateHash creates SHA-256 hash based on Block
 func (b *HashBlock) CreateHash() {
 	nonce := []byte(strconv.FormatInt(b.CreatedAt, 10))
-	hash := sha256.Sum256(bytes.Join([][]byte{b.PrevBlockHash, b.Data, nonce}, []byte{}))
+	hash := sha256.Sum256(bytes.Join([][]byte{b.Data, b.PrevBlockHash, nonce}, []byte{}))
 	b.Hash = hash[:]
+}
+
+// CreateBlock creates a Block
+func CreateBlock(data string, prevBlockHash []byte) *HashBlock {
+	block := &HashBlock{[]byte(data), []byte{}, prevBlockHash, time.Now().Unix()}
+	block.CreateHash()
+	return block
 }
